6506436dcf402e0c8c4b8b339376fc3dc573a1b3
Stiven <Stiven@DESKTOP-4CA2HN8>
2025-07-14 09:59:20 -0600

    se agrego el context menu y se cambiaron un poco las validaciones

--- gestor de facturas/CapaInterfaz/Form1.Designer.cs
+++ gestor de facturas/CapaInterfaz/Form1.Designer.cs
@@ -36,80 +36,93 @@
             txtcantidad = new TextBox();
             txtprecio = new TextBox();
             dgvproductos = new DataGridView();
+            ctmproductos = new ContextMenuStrip(components);
+            eliminarFilaToolStripMenuItem = new ToolStripMenuItem();
             btnagregar = new Button();
             lbltotal = new Label();
             errorProvider1 = new ErrorProvider(components);
             ((System.ComponentModel.ISupportInitialize)dgvproductos).BeginInit();
+            ctmproductos.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)errorProvider1).BeginInit();
             SuspendLayout();
             // 
             // lblnombre
             // 
             lblnombre.AutoSize = true;
-            lblnombre.Location = new Point(126, 40);
+            lblnombre.Location = new Point(144, 53);
             lblnombre.Name = "lblnombre";
-            lblnombre.Size = new Size(51, 15);
+            lblnombre.Size = new Size(64, 20);
             lblnombre.TabIndex = 0;
             lblnombre.Text = "Nombre";
             // 
             // lblcantidad
             // 
             lblcantidad.AutoSize = true;
-            lblcantidad.Location = new Point(122, 85);
+            lblcantidad.Location = new Point(139, 113);
             lblcantidad.Name = "lblcantidad";
-            lblcantidad.Size = new Size(55, 15);
+            lblcantidad.Size = new Size(69, 20);
             lblcantidad.TabIndex = 1;
             lblcantidad.Text = "Cantidad";
             // 
             // lblprecio
             // 
             lblprecio.AutoSize = true;
-            lblprecio.Location = new Point(130, 130);
+            lblprecio.Location = new Point(149, 173);
             lblprecio.Name = "lblprecio";
-            lblprecio.Size = new Size(40, 15);
+            lblprecio.Size = new Size(50, 20);
             lblprecio.TabIndex = 2;
             lblprecio.Text = "Precio";
             // 
             // txtnombre
             // 
-            txtnombre.Location = new Point(208, 38);
-            txtnombre.Margin = new Padding(3, 2, 3, 2);
+            txtnombre.Location = new Point(238, 51);
             txtnombre.Name = "txtnombre";
-            txtnombre.Size = new Size(314, 23);
+            txtnombre.Size = new Size(358, 27);
             txtnombre.TabIndex = 3;
             // 
             // txtcantidad
             // 
-            txtcantidad.Location = new Point(206, 82);
-            txtcantidad.Margin = new Padding(3, 2, 3, 2);
+            txtcantidad.Location = new Point(235, 109);
             txtcantidad.Name = "txtcantidad";
-            txtcantidad.Size = new Size(316, 23);
+            txtcantidad.Size = new Size(361, 27);
             txtcantidad.TabIndex = 4;
             // 
             // txtprecio
             // 
-            txtprecio.Location = new Point(206, 125);
-            txtprecio.Margin = new Padding(3, 2, 3, 2);
+            txtprecio.Location = new Point(235, 167);
             txtprecio.Name = "txtprecio";
-            txtprecio.Size = new Size(316, 23);
+            txtprecio.Size = new Size(361, 27);
             txtprecio.TabIndex = 5;
             // 
             // dgvproductos
             // 
             dgvproductos.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            dgvproductos.Location = new Point(77, 185);
-            dgvproductos.Margin = new Padding(3, 2, 3, 2);
+            dgvproductos.ContextMenuStrip = ctmproductos;
+            dgvproductos.Location = new Point(88, 247);
             dgvproductos.Name = "dgvproductos";
             dgvproductos.RowHeadersWidth = 51;
-            dgvproductos.Size = new Size(542, 169);
+            dgvproductos.Size = new Size(619, 225);
             dgvproductos.TabIndex = 6;
+            // 
+            // ctmproductos
+            // 
+            ctmproductos.ImageScalingSize = new Size(20, 20);
+            ctmproductos.Items.AddRange(new ToolStripItem[] { eliminarFilaToolStripMenuItem });
+            ctmproductos.Name = "ctmproductos";
+            ctmproductos.Size = new Size(211, 56);
+            // 
+            // eliminarFilaToolStripMenuItem
+            // 
+            eliminarFilaToolStripMenuItem.Name = "eliminarFilaToolStripMenuItem";
+            eliminarFilaToolStripMenuItem.Size = new Size(210, 24);
+            eliminarFilaToolStripMenuItem.Text = "Eliminar fila";
+            eliminarFilaToolStripMenuItem.Click += eliminarFilaToolStripMenuItem_Click;
             // 
             // btnagregar
             // 
-            btnagregar.Location = new Point(308, 159);
-            btnagregar.Margin = new Padding(3, 2, 3, 2);
+            btnagregar.Location = new Point(352, 212);
             btnagregar.Name = "btnagregar";
-            btnagregar.Size = new Size(82, 22);
+            btnagregar.Size = new Size(94, 29);
             btnagregar.TabIndex = 7;
             btnagregar.Text = "Agregar";
             btnagregar.UseVisualStyleBackColor = true;
@@ -118,9 +131,9 @@
             // lbltotal
             // 
             lbltotal.AutoSize = true;
-            lbltotal.Location = new Point(526, 161);
+            lbltotal.Location = new Point(601, 215);
             lbltotal.Name = "lbltotal";
-            lbltotal.Size = new Size(31, 15);
+            lbltotal.Size = new Size(40, 20);
             lbltotal.TabIndex = 8;
             lbltotal.Text = "total";
             // 
@@ -130,10 +143,10 @@
             // 
             // Form1
             // 
-            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleDimensions = new SizeF(8F, 20F);
             AutoScaleMode = AutoScaleMode.Font;
             BackColor = SystemColors.ButtonFace;
-            ClientSize = new Size(700, 363);
+            ClientSize = new Size(800, 484);
             Controls.Add(lbltotal);
             Controls.Add(btnagregar);
             Controls.Add(dgvproductos);
@@ -143,10 +156,10 @@
             Controls.Add(lblprecio);
             Controls.Add(lblcantidad);
             Controls.Add(lblnombre);
-            Margin = new Padding(3, 2, 3, 2);
             Name = "Form1";
             Text = "Productos";
             ((System.ComponentModel.ISupportInitialize)dgvproductos).EndInit();
+            ctmproductos.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)errorProvider1).EndInit();
             ResumeLayout(false);
             PerformLayout();
@@ -164,5 +177,7 @@
         private Button btnagregar;
         private Label lbltotal;
         private ErrorProvider errorProvider1;
+        private ContextMenuStrip ctmproductos;
+        private ToolStripMenuItem eliminarFilaToolStripMenuItem;
     }
 }
--- gestor de facturas/CapaInterfaz/Form1.cs
+++ gestor de facturas/CapaInterfaz/Form1.cs
@@ -1,71 +1,59 @@
 using gestor_de_facturas.CapaDatos;
 using gestor_de_facturas.CapaLogica;
+using System.Windows.Forms;
+using static System.Runtime.InteropServices.JavaScript.JSType;
 
 namespace gestor_de_facturas
 {
     public partial class Form1 : Form
     {
         private LogicaFactura logica = new LogicaFactura();
-        private ErrorProvider errorProvider = new ErrorProvider(); 
-        public Form1()  
+        private ErrorProvider errorProvider = new ErrorProvider();
+        public Form1()
         {
             InitializeComponent();
         }
-        
+
         //validacion de campos de errorprovider
         private bool ValidarCampos()
         {
             bool esvalido = true;
             errorProvider.Clear();
-            
-             if (string.IsNullOrWhiteSpace(txtnombre.Text)) //valida si el nombre esta vacio o no
-            {
-                errorProvider.SetError(txtnombre, "!!!El nombre no puede quedar vacio!!!");// si esta vacio el errorProvider da este mensaje 
-                esvalido = false;
-            }
-            
-             if (string.IsNullOrWhiteSpace(txtcantidad.Text))//  valida si la cantidad se encuentra vacia
-            {
-                errorProvider.SetError(txtcantidad, "!!!La cantidad no puede quedar vacia!!!");// muestra este mensaje de advertencia si se encuentra vacia
-                esvalido = false;
-            }
-            else
-            {
-                //valida que la cantidad sea correcta
-                if (!int.TryParse(txtcantidad.Text, out int cantidad) || cantidad <= 0)
-                {
-                    errorProvider.SetError(txtcantidad, "!!!La cantidad debe de ser un numero entero!!!");// si se escribe una cantidad incorrecta muestra este mensaje de advertencia
-                    esvalido = false;
-                }
 
-            }
-            if (string.IsNullOrWhiteSpace(txtprecio.Text))//valida que el precio no este vacio
+            if (string.IsNullOrWhiteSpace(txtnombre.Text)) //valida si el nombre esta vacio o no
             {
-                errorProvider.SetError(txtprecio, "!!!El precio no puede quedar vacio!!!");// y si lo esta da este mensaje de advertencia
+                errorProvider.SetError(txtnombre, "El nombre no puede quedar vacio");// si esta vacio el errorProvider da este mensaje 
                 esvalido = false;
             }
-            else
+            else if (!System.Text.RegularExpressions.Regex.IsMatch(txtnombre.Text, @"^[a-zA-Z\s]+$"))
             {
-                if (!decimal.TryParse(txtprecio.Text, out decimal precio) || precio<= 0)//valida que el precio sea el correcto
-                {
-                    errorProvider.SetError(txtprecio, "!!!El precio debe de ser mayor a 0!!!");// y este mensaje es el que muestra 
-                    esvalido = false;
-                }   
-
+                errorProvider.SetError(txtnombre, "El nombre solo puede tener letras ");// si esta vacio el errorProvider da este mensaje 
+                esvalido = false;
             }
-            return esvalido;        
+            if (!int.TryParse(txtcantidad.Text, out int cantidad) || cantidad <= 0)
+            {
+                errorProvider.SetError(txtcantidad, "La cantidad debe de ser un numero entero");// si se escribe una cantidad incorrecta muestra este mensaje de advertencia
+                esvalido = false;
+            }
+
+            if (!decimal.TryParse(txtprecio.Text, out decimal precio) || precio <= 0)//valida que el precio sea el correcto
+            {
+                errorProvider.SetError(txtprecio, "El precio debe ser un numero y mayor a 0");// y este mensaje es el que muestra 
+                esvalido = false;
+            }
+            return esvalido;
         }
 
         private void btnagregar_Click(object sender, EventArgs e)
         {
-            if(!ValidarCampos())
+            if (!ValidarCampos())
             {
                 return;
             }
             ProductoFactura producto = new ProductoFactura
             {
                 Nombre = txtnombre.Text,
-                Cantidad =int.Parse(txtcantidad.Text),
+                Cantidad = int.Parse(txtcantidad.Text),
                 Precio = decimal.Parse(txtprecio.Text)
 
             };
@@ -74,7 +62,37 @@ namespace gestor_de_facturas
             dgvproductos.DataSource = null;
             dgvproductos.DataSource = logica.ListaProductos;
 
-            lbltotal.Text= "Total: "+ logica.CalcularTotal().ToString("n2");
+            lbltotal.Text = "Total: " + logica.CalcularTotal().ToString("n2");
+        }
+
+
+        private void dgvproductos_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Right)
+            {
+                var hit = dgvproductos.HitTest(e.X, e.Y);
+                if (hit.RowIndex >= 0) //si se selecciona una linea en el datagrind
+                {
+                    dgvproductos.ClearSelection(); //se limpia esa linea
+                    dgvproductos.Rows[hit.RowIndex].Selected = true;
+                }
+            }
+        }
+
+        private void eliminarFilaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            if (dgvproductos.SelectedRows.Count > 0)
+            {
+                DataGridViewRow filaseleccionada = dgvproductos.SelectedRows[0];
+                int indice = filaseleccionada.Index;
+
+                logica.ListaProductos.RemoveAt(indice);
+
+                dgvproductos.DataSource = null;
+                dgvproductos.DataSource = logica.ListaProductos;
+
+                lbltotal.Text = "Total: " + logica.CalcularTotal().ToString("n2");
+            }
         }
     }
 }
